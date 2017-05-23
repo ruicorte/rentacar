25edb24c791531ea33e7b664901d3f3664c5d65a
David75FX <candido.david@gmail.com>
2017-05-23 17:37:17 +0100

    casa do david

--- application/views/frota/index.php
+++ application/views/frota/index.php
@@ -1,6 +1,12 @@
-<h1>página com automóveis</h1>
-
-<div>
-	<?php $this->load->view('frota/table'); ?>
-
+<div class="container" style="margin-top:50px;">
+	<div class="row">
+		<div class="col-xs-12">
+			<?php $this->load->view('frota/pesquisa'); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<?php $this->load->view('frota/table'); ?>
+		</div>
+	</div>
 </div>
--- application/views/frota/pesquisa.php
+++ application/views/frota/pesquisa.php
@@ -1,21 +1,41 @@
-<div class="container" style="margin-top:50px;">
-  <div class="row">
-    <div class="col-xs-12">
-      <div class="input-group">
+<form role="form">
+  <div class="col-xs-12">
+    <div class="form-group">
+      <div class="input-group">       
+        <span class="input-group-btn">
+          <select class="btn btn-warning">
+            <option>Modelo</option>
+            <option>Matrícula</option>
+            <option>Fabricante</option>
+          </select>
+        </span>
+        <input type="text" class="form-control" placeholder="Termo de pesquisa">
         <div class="input-group-btn">
-          <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Action
-          </button>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#" onselect="console.log('modelo')">Modelo</a>
-            <a class="dropdown-item" href="#" onselect="console.log('matricula')">Matricula</a>
-            <a class="dropdown-item" href="#" onselect="console.log('fabricante')">Fabricante</a>
-            <!--<div role="separator" class="dropdown-divider"></div>-->
-          </div>
+          <button type="button" class="btn btn-primary" >Pesquisa</button>
         </div>
-        <input type="text" class="form-control" aria-label="Text input with dropdown button">
       </div>
     </div>
-  </div>  
-</div>
+  </div>
+</form>
 
+
+<!--
+  <div class="form-group">
+    <label>But, I want it to look more like this (this uses a input-group-btn)</label>
+    <div class="input-group">
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Inches <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+          <li><a href="#">Inches</a></li>
+          <li><a href="#">Feet</a></li>
+          <li><a href="#">mm</a></li>
+        </ul>
+      </div>                 
+      <input type="text" class="form-control">
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-default" >Inches </button>
+      </div>
+    </div>
+  </div>
+  </div>
+</form>-->
