ee9b667646dbb10aea5916789d600a5d73111c7c
ruicorte <ruicorte@gmail.com>
2017-05-23 11:03:00 +0100

    controller frota corrigido

--- application/controllers/Frota.php
+++ application/controllers/Frota.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Publico extends CI_Controller {
+class Frota extends CI_Controller {
 
 	/**
 	 * [Pesquisa description]
