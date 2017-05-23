d3fb4fd525791c61f95883186753c2719660da69
nelsonhrodrigues <nelsonhrodrigues@gmail.com>
2017-05-23 11:02:52 +0100

    nav repair

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -16,14 +16,14 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <?php echo setMenuItemActive( $active_menu == "sobre"); ?><a href="<?php echo base_url();?>Publico/index">Sobre</a>
+                    <li
+                        <?php echo setMenuItemActive( $active_menu == "sobre"); ?>><a href="<?php echo base_url();?>Publico/sobre">Sobre</a>
                     </li>
-                    <li>
-                        <?php echo setMenuItemActive( $active_menu == "frota"); ?><a href="<?php echo base_url();?>Frota/index">Frota automóvel</a>
+                    <li
+                        <?php echo setMenuItemActive( $active_menu == "frota"); ?>><a href="<?php echo base_url();?>Frota/index">Frota automóvel</a>
                     </li>
-                    <li>
-                       <?php echo setMenuItemActive( $active_menu == "contacto"); ?><a href="<?php echo base_url();?>Publico/contacto">Contacto</a>>
+                    <li
+                       <?php echo setMenuItemActive( $active_menu == "contacto"); ?>><a href="<?php echo base_url();?>Publico/contacto">Contacto</a>>
                     </li>
                 </ul>
             </div>
