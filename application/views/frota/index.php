c9d3d7959581fc74da284e4bd1769d49c37bbef3
nelsonhrodrigues <nelsonhrodrigues@gmail.com>
2017-05-23 11:28:29 +0100

    database

---
+++ application/views/frota/index.php
@@ -0,0 +1,9 @@
+<h1>página com automóveis</h1>
+
+<div>
+	<?php $this->load->view('frota/table'); ?>
+<<<<<<< HEAD:application/views/frota/frota.php
+</div>
+=======
+</div>
+>>>>>>> bd53d345ee21004060cb8a5c63f5f7067f208e2f:application/views/frota/index.php
--- application/views/frota/table.php
+++ application/views/frota/table.php
@@ -7,18 +7,19 @@
     <tbody>
 
      <?php foreach ($frota as $car) {
-      $titulo = $car->disponibilidade; 
-      $nome = $car->matricula; 
-      $data_publicacao = $car->cor; 
-      $data_publicacao = $car->modelo; 
-      $data_publicacao = $car->fabricante; 
-      echo ("<tr><th>$car->fabricante</th><th>$car->modelo</th><th> $car->cor</th><th>$car->matricula</th><th>$car->disponibilidade</th><th><a href='editBar.php?id=$car->$id'> <i class='fa fa-pencil-square-o' aria-hidden='true'></i> </a><a href='asMelhores.php?apaga=$id'> <i class='fa fa-times' aria-hidden='true' style='color:darkred'></i> </a></th></tr>");}?>
+      $id = $car->id; 
+      $disponibilidade = $car->disponibilidade; 
+      $matricula = $car->matricula; 
+      $cor = $car->cor; 
+      $modelo = $car->modelo; 
+      $fabricante = $car->fabricante; 
+      echo ("<tr><th>$fabricante</th><th>$modelo</th><th> $cor</th><th>$matricula</th><th>$disponibilidade</th><th><a href='editBar.php?id=$id'> <i class='fa fa-pencil-square-o' aria-hidden='true'></i> </a><a href='asMelhores.php?apaga=$id'> <i class='fa fa-times' aria-hidden='true' style='color:darkred'></i> </a></th></tr>");}?>
 <?php echo ("<tr><th></th><th></th><th></th><th></th><th><a href='adicionaBar.php'> <i class='fa fa-plus' aria-hidden='true'></i> </a> </th></tr>");?>
       <tbody>
       </table>
-       <?php echo $search_pagination;?>
+       <?php //echo $search_pagination;?>
       <hr>
-      <h3>total books: <?php echo $total_rows;?></h3>
+      <h3>total Cars: <?php echo $total_rows;?></h3>
       <?php echo "<hr>";
       ?>
    </div><!-- /.col-md-12 -->
