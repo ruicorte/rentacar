df97ecbe6b11db0e6c6546bde6813ede6809101a
ruicorte <ruicorte@gmail.com>
2017-05-23 09:58:02 +0100

    implementação da estrutura base da página

--- application/controllers/Publico.php
+++ application/controllers/Publico.php
@@ -3,10 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Publico extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		
+	}
 	
 	public function index()
-	{
-		$data['teste'] = 'isto é um teste';
-		$this->load->view('publico', $data);
+	{		
+		$data['titulo'] = 'BVRC - home';
+		$data['page'] = 'publico/publico';
+		$this->load->view('html', $data);
+	}
+
+	/**
+	 * página de informações gerais
+	 * @return [type] [description]
+	 */
+	public function sobre(){
+
+	}
+
+	/**
+	 * página de contacto que inclui um formulário de contacto (codeigniter email)
+	 * @return [type] [description]
+	 */
+	public function contacto(){
+
 	}
 }
---
+++ application/views/html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view('template/head'); ?>
+</head>
+
+<body>
+	<?php $this->load->view('template/nav'); ?>
+	<?php $this->load->view($page); ?>
+	<?php $this->load->view('template/footer'); ?>
+</body>
+
+</html>
