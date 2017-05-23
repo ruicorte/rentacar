482c16e476803a815250455c655586c2385e9e1c
ruicorte <ruicorte@gmail.com>
2017-05-23 11:44:52 +0100

    formulário de contacto criado

--- application/views/frota/frota.php
+++
@@ -1,5 +0,0 @@
-<h1>página com automóveis</h1>
-
-<div>
-	<?php $this->load->view('frota/table'); ?>
-</div>
--- application/views/publico/contacto.php
+++ application/views/publico/contacto.php
@@ -1,4 +1,47 @@
-<h1>contacto</h1>
-<br><br>
+<div class="container" style="margin-top:50px;">
+	<div class="row">
+		<div class="col-xs-12">
+			<form class="form-horizontal">
+				<fieldset>
 
-<p>aqui vai levar um formulário</p>
+					<!-- Form Name -->
+					<legend>Contacte a Empresa</legend>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="nome_email">Nome</label>  
+						<div class="col-md-4">
+							<input id="nome_email" name="nome_email" type="text" placeholder="" class="form-control input-md" required="">
+
+						</div>
+					</div>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="email">Email</label>  
+						<div class="col-md-4">
+							<input id="email" name="email" type="text" placeholder="" class="form-control input-md" required="">
+
+						</div>
+					</div>
+
+					<!-- Textarea -->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="mensagem_email">Mensagem</label>
+						<div class="col-md-4">                     
+							<textarea class="form-control" id="mensagem_email" name="mensagem_email"></textarea>
+						</div>
+					</div>
+
+					<!-- Button -->
+					<div class="form-group">
+						<div class="col-md-8 text-right">
+							<button id="submit_btn_email" name="submit_btn_email" class="btn btn-primary">Enviar</button>
+						</div>
+					</div>
+
+				</fieldset>
+			</form>
+		</div>
+	</div>
+</div>
