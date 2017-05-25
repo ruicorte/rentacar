52a62e66fb0bc97bf5e8a14fb78a5d0bba52a73c
nelsonhrodrigues <nelsonhrodrigues@gmail.com>
2017-05-25 23:12:51 +0100

    update para o caso de nao haver matricula

--- application/views/frota/remover.php
+++ application/views/frota/remover.php
@@ -4,14 +4,14 @@
 		<div class="col-xs-12">
 			<!-- Button (Double) -->
 			<div class="form-group">
-				<label class="col-md-4 control-label" for="cancel_button">Confirmar eliminar Veiculo <?=$matricula->matricula;?></label>
+				<label class="col-md-4 control-label" for="cancel_button">Confirmar eliminar Veiculo <?=isset($matricula->matricula) ? $matricula->matricula:'xx-xx-xx';?></label>
 				<div class="col-md-8">
 					<form  style="display:inline" method="post" action="<?=base_url('frota/index');?>">
 	    				<button id="submit_cancel" name="submit_cancel" class="btn btn-warning">Cancelar</button>
 					</form>
-					<form  style="display:inline" method="post" action="<?=base_url('frota/remover/'.$matricula->id)?>">
+					<?=isset($matricula->matricula) ? '<form  style="display:inline" method="post" action="'.base_url("frota/remover/".$matricula->id).'">
 	    				<button id="submit_eliminar" name="submit_eliminar" value="TRUE" class="btn btn-primary">Eliminar</button>
-					</form>					
+					</form>				':'';?>	
 				</div>
 			</div>
 		</div>
