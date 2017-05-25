5dcfc7c6242cee2f11b0f244da7c6fa1643a92a1
ruicorte <ruicorte@gmail.com>
2017-05-25 11:48:00 +0100

    continues
    falta:
    
    - paginação
    - edição de automóvel
    - bug (frota)

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="<?php echo base_url();?>Publico/index">
-                    <img src="<?php echo base_url('assets/rentacar.jpg'); ?>" alt="">
+                    <img src="<?php echo base_url('assets/rentacar.png'); ?>" alt="">
                 </a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
