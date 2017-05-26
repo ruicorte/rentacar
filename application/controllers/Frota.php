05329e31ef1ddf4750d4d13274f1f8bce3a38637
David75FX <candido.david@gmail.com>
2017-05-26 10:43:26 +0100

    correção no Frota.php

--- application/controllers/Frota.php
+++ application/controllers/Frota.php
@@ -51,10 +51,11 @@ class Frota extends CI_Controller {
 		$data['page'] 		 = 'frota/formulario_automovel';
 		$data['active_menu'] = 'frota';
 
-		if($this->input->post()){
+		if(	$this->input->post()){
 			$this->load->helper('form');
 			$this->load->library('form_validation');
 			$this->form_validation->set_error_delimiters('<span class="help-inline text-danger"> * ', '</span>');
+			
 			$config = [
 						[
 						'field'		=> 'matricula',
@@ -124,7 +125,8 @@ class Frota extends CI_Controller {
 										'required' 	   => 'obrigatório: %s do automóvel'
 										]
 						]
-			];
+					];
+					
 		$this->form_validation->set_rules($config);
 		if($this->form_validation->run()){
 			$this->load->model('frota_model', 'frota');
