26fe99e2e7779d42542944cc3ea042e70897e331
David75FX <candido.david@gmail.com>
2017-05-26 09:03:10 +0100

    correção no tableEmail.php

--- application/views/frota/tableEmail.php
+++ application/views/frota/tableEmail.php
@@ -1,9 +1,11 @@
 <div class="col-md-12">
-      <?php
+  
+  <?php
   if(isset($_SESSION['emailstatus'])) {
     echo $_SESSION['emailstatus'];
     unset($_SESSION['emailstatus']);
   }?>
+
   <table class="text-center table table-bordered table-hover table-responsive">
     <thead class="text-center">
       <tr>
@@ -17,27 +19,32 @@
     </thead>
     <tbody>
       <?php 
-      foreach ($this->session->email as $mensagem): ?>
+        foreach ($this->session->email as $mensagem): 
+      ?>
       <tr>
         <td><?=$mensagem->id?></td>
         <td><?=$mensagem->nome?></td>
         <td><?=$mensagem->email?></td>
         <td><?=$mensagem->mensagem?></td>
-        <td><?php 
+        <td>
+          <?php 
           $data = new DateTime($mensagem->date);
           $data = $data->format('Y-m-d H:i:s');
-          echo $data;?></td>
+          echo $data;
+          ?>
+        </td>
           <td>
             <form  style="display:inline" method="post" action="<?=base_url('frota/listarEmail/'.$mensagem->id)?>">
               <button id="submit_eliminar" name="submit_eliminar" value="TRUE" class="btn btn-danger">Apagar <span class='fa fa-times' aria-hidden='true'></span></button>
-            </form> </td>
-          </tr>
-        <?php endforeach; ?>
-        <tbody>
-        </table>
-        <?php //echo $search_pagination;?>
-        <hr>
-        <?php echo "<hr>";?>
-      </div><!-- /.col-md-12 -->
+            </form> 
+          </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  <?php //echo $search_pagination;?>
+  <hr>
+  <?php echo "<hr>";?>
+</div><!-- /.col-md-12 -->
 
 
