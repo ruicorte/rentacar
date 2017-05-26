d449e6b19e1965eabfcc3cf7e9b5da8e870fdae4
David75FX <candido.david@gmail.com>
2017-05-26 11:02:24 +0100

    correção do Publico.php

--- application/controllers/Publico.php
+++ application/controllers/Publico.php
@@ -49,7 +49,7 @@ class Publico extends CI_Controller {
 								// o %s e para escrever o label
 								'required'			  => 'É obrigatorio indicar um %s',
 								//erro se existe caracteres especiais
-								'alpha_numeric_spaces'=>'contem caracteres invalidos no %s',
+								'alpha_numeric_spaces'=> 'contem caracteres invalidos no %s',
 								//erro se excede o tamanho maximo
 								'max_length'          => 'Excedeu o maximo de 50 caracteres no %s'
 								)
@@ -70,7 +70,7 @@ class Publico extends CI_Controller {
 				'rules' => 'required|alpha_numeric_spaces|max_length[500]',
 				'errors'=> array(
 					 			'required'			  => 'É obrigatorio indicar um %s',
-								'alpha_numeric_spaces'=>'contem caracteres invalidos na %s',
+								'alpha_numeric_spaces'=> 'contem caracteres invalidos na %s',
 								'max_length'		  => 'Excedeu o maximo de 500 caracteres na %s'
 								)
 				)
