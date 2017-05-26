2d0c6cc6ac29287fa3d73c0c6e62aefdbc04cf7b
nelsonhrodrigues <nelsonhrodrigues@gmail.com>
2017-05-26 11:23:50 +0100

    SOBRE

--- application/views/publico/sobre.php
+++ application/views/publico/sobre.php
@@ -7,7 +7,8 @@
 					<h4><strong>Missão</strong></h4>
 				</div>
 				<div class="panel-body">
-					<p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</strong></p>
+					<p><strong>Mobilidade é a nossa razão de ser. Satisfação do cliente é a nossa motivação. O cliente é a nossa razão de existir. Acreditamos na solidez ética, na honestidade e na integridade como princípios do comportamento empresarial, interno e externo. Acreditamos na inovação como base de desenvolvimento dos negócios. Somos responsáveis perante a comunidade pelo uso eficiente e ponderado dos recursos nos Ecossistemas onde nos movimentamos. Acreditamos que o mais crítico dos factores de sucesso é a capacidade para atrair os melhores, os recursos humanos mais qualificados e motivados. Queremos, por isso, ser reconhecidos como um bom lugar para cada um exercer os seus talentos e ser retribuído com justiça.
+					Sentimo-nos responsáveis perante a comunidade pela contribuição para minorar os problemas que afectam as minorias desprotegidas e as pessoas com necessidades especiais.</strong></p>
 				</div>
 			</div>
 		</div>
@@ -43,7 +44,8 @@
 					<h4><strong>História</strong></h4>
 				</div>
 				<div class="panel-body">
-					<strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</strong>
+					<strong>AUTO RENT A CAR é uma empresa 100% Portuguesa e tem presença no território nacional desde 1989. A actual estrutura acionista resulta de uma join venture entre o Grupo AUTO RENT A CAR e a companhia ACIN. A qualidade do serviço e a vanguarda da tecnologia são premissas essenciais para garantir a satisfação dos nossos clientes na escolha das nossas soluções de mobilidade.
+					Mobilidade é a nossa razão de ser. Satisfação do cliente é a nossa motivação.</strong>
 				</div>
 			</div>
 		</div>
