08c1ed0bd45d7ed5365fc5af850617cf9f819849
David75FX <candido.david@gmail.com>
2017-05-26 10:53:45 +0100

    correção de Modelos_model.php

--- application/models/Modelos_model.php
+++ application/models/Modelos_model.php
@@ -1,12 +1,17 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Modelos_model extends CI_Model {
+
 	public function __construct() {
+		
 		parent::__construct();
+
 		$this->load->database();
 	}
 
 	public function getAll(int $fabricantes_id = 0):array {
+		
 		if($fabricantes_id > 0)
 		
 		$this->db->where('fabricante_id', $fabricantes_id);
