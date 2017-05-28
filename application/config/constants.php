ca7f0c118681a5e581744debde94764d33361a2d
nelsonhrodrigues <nelsonhrodrigues@gmail.com>
2017-05-28 21:53:36 +0100

    files for the pagination

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,7 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/**Custom constants from developer*/
+
+defined('ITEMS_PER_PAGE')      OR define('ITEMS_PER_PAGE', 10); // number of items to be shown in pagination
