d570fdf96a2dd147e9dd2d1b76e6b3a8b1f5f857
David75FX <candido.david@gmail.com>
2017-05-30 11:42:49 +0100

    correção de index.php-publico

--- application/views/publico/index.php
+++ application/views/publico/index.php
@@ -1,6 +1,6 @@
     <!-- Page Content -->
     <div class="container" style="margin-top:50px;">
-
+    
         <!-- Heading Row -->
         <div class="row">
             <div class="col-md-8">
@@ -8,9 +8,9 @@
             </div>
             <!-- /.col-md-8 -->
             <div class="col-md-4">
-                <h1>Rent-a-Car do David!</h1>
+                <h1>Brava Valley Rent-a-Car</h1>
                 <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-                <a class="btn btn-primary btn-lg" href="#">Call to Action!</a>
+                <a class="btn btn-primary btn-lg" href="#">Liga-nos e estamos a vossa espera !</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -22,7 +22,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="well text-center">
-                    This is a well that is a great spot for a business tagline or phone number for easy access!
+                    <p><strong>Este é um ótimo local para um negócio de slogan ou número de telefone para acesso fácil !</strong></p>
                 </div>
             </div>
             <!-- /.col-lg-12 -->
@@ -32,21 +32,21 @@
         <!-- Content Row -->
         <div class="row">
             <div class="col-md-4">
-                <h2>Heading 1</h2>
+                <h2>Fundação</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-                <a class="btn btn-default" href="#">More Info</a>
+                <a class="btn btn-primary" href="#">A nossa Historia</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
-                <h2>Heading 2</h2>
+                <h2>Mapa</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-                <a class="btn btn-default" href="#">More Info</a>
+                <a class="btn btn-primary" href="#">Clica-nos do nosso local</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
-                <h2>Heading 3</h2>
+                <h2>Qualidade de Empresa</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-                <a class="btn btn-default" href="#">More Info</a>
+                <a class="btn btn-primary" href="#">Informação da Empresa</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -56,7 +56,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2017</p>
+                    
                 </div>
             </div>
         </footer>
